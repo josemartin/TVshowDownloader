88560f28f71658962b2d041e04cec68136a105d7
Jose Martin <jose.martin.burgos@gmail.com>
2014-04-15 18:56:23 +0200

    Improved code style

--- tsd.py
+++ tsd.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python
+"""
+
+@author J. Martin 
+April 2014
+TV show downloader
+
+"""
 # -*- coding: utf-8 -*-
 
 __author__ = "J. Martin"
 __license__ = """GPL v3. See LICENCE for full text.
-Includes software parts by XBMC team, and feedparser, check respective files for licence details"""
+Includes software parts by XBMC team, and feedparser.
+Check respective files for licence details"""
 
 import time
 from subtitles import Subtitles
@@ -11,40 +18,41 @@ from chapters import Chapters
 from Comm import Log
 
 # Paths & files
-showFile = "shows.cfg"
-global logfile 
-logfile = "tvshowlog.txt"
-subfile = "subpen.txt"
-subfolder =	"/storage/USB/Descargas/Subtitles"
+SHOWFILE = "shows.cfg"
+LOGFILE = "tvshowlog.txt"
+SUBFILE = "subpen.txt"
+SUBFOLDER = "/storage/USB/Descargas/Subtitles"
 
 
-if __name__=='__main__':
-	# Initialize log
-	log = Log (logfile)
+if __name__ == '__main__':
+    # pylint: disable-msg=C0103
+    
+    # Initialize log
+    log = Log (LOGFILE)
 
-	# Initialize subtitler
-	subtitler = Subtitles (subfile, subfolder, log)
-	subtitler.loadPendingSubs()
+    # Initialize subtitler
+    subtitler = Subtitles (SUBFILE, SUBFOLDER, log)
+    subtitler.loadPendingSubs()
 
-	# Initial log traces
-	log.write("Runned at " + time.strftime("%d/%m/%Y %H:%M:%S"),0)
-	log.write("",0)
-	log.write("Checking for new episodes...",0)
-	
-	# Initialize chapterer
-	chapterer = Chapters (showFile, subtitler, log)
-	chapterer.loadShows()
+    # Initial log traces
+    log.write("Runned at " + time.strftime("%d/%m/%Y %H:%M:%S"), 0)
+    log.write("", 0)
+    log.write("Checking for new episodes...", 0)
+    
+    # Initialize chapterer
+    chapterer = Chapters (SHOWFILE, subtitler, log)
+    chapterer.loadShows()
 
-	# Check for new shows
-	chapterer.checkShows()
+    # Check for new shows
+    chapterer.checkShows()
 
-	log.write("",0)
-	log.write("Checking for subtitles...",0)
-	
-	# Check for new subtitles
-	subtitler.checkSubtitles()
-	subtitler.writePendingSubs()
+    log.write("", 0)
+    log.write("Checking for subtitles...", 0)
+    
+    # Check for new subtitles
+    subtitler.checkSubtitles()
+    subtitler.writePendingSubs()
 
 
-	log.write("Done",0)
-	log.write("---------------------------------------",0)
+    log.write("Done", 0)
+    log.write("---------------------------------------", 0)
