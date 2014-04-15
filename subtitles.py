d5facbf7917832e30ade5496984fb5fecd76fa76
Jose Martin <jose.martin.burgos@gmail.com>
2014-04-15 17:16:05 +0200

    Separate subtitles functions from main file. Renamed main program to tsd.py.

---
+++ subtitles.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+#===============================================================================
+# 
+#  @author J. Martin 
+#  April 2014
+# 
+#  Manages the TVshowDownloader subtitles
+#
+#===============================================================================
+
+import re
+import os
+
+# Subtitles service 
+import subService
+
+#-------------------------------------------------------------------
+def sendNotification (text):
+    os.system("echo \"" + text + "\" | /usr/bin/sendxmpp -t -u ceoserverpi -o gmail.com jose.martin.burgos")
+#-------------------------------------------------------------------
+def writeToLog (text, level):
+    f = open ("logfile", "a")
+    
+    a=""
+    
+    for i in range(level):
+        a = a + "-"
+    
+    print >> f, a + text
+    print  a + text
+    f.close()
+#-------------------------------------------------------------------
+
+class Subtitles:
+    '''
+    Handlles the subtitles part of TVshowDownloader
+    '''
+    def __init__(self, filename, downloadFolder):
+        '''
+        Stores the pending subtitles filename
+        '''
+        self.filename = filename
+        self.downloadFolder = downloadFolder
+        self.subpending = []
+        
+    def loadPendingSubs (self):
+        '''
+        Loads the subs from the supplied file
+        The file shoud contain a chapter by line with the format show,season,chapter
+
+        '''
+        subfile = open(self.filename)
+        while True:
+			line = subfile.readline()
+			if not line:
+				break
+				
+			parts = line.split(',')
+			self.subpending.append([parts[0], int(parts[1]), int(parts[2])])
+        subfile.close()
+    def writePendingSubs (self):
+    	'''
+    	Writes the pending sub to the supplied filename.
+    	See loadPendingSubs() for file syntax
+    	'''
+    	subfileo = open(self.filename,"w")
+        for entry in self.subpending:
+			print >> subfileo, entry[0] + "," + str(entry[1]) + "," + str(entry[2])
+        subfileo.close()
+    def addSub(self, entry):
+        '''
+        Adds a entry to the pending subtitles
+        '''
+        self.subpending.append (entry)
+
+    def checkSubtitles (self):
+    	'''
+
+    	'''
+
+    	for entry in self.subpending:
+			writeToLog( "Looking for sub to " + entry[0] + " " + str(entry[1]).zfill(2) + "x" + str(entry[2]).zfill(2),1)
+			
+			subSearch = subService.search_subtitles("", "", entry[0],"",str(entry[1]),str(entry[2]),"","","Spanish","","","")
+			
+			if len(subSearch[0]) > 0:
+				flag = 0
+				for i in range(len(subSearch[0])):
+					tipo = re.compile( "\((.*)\)" ).search( str(subSearch[0][i]["filename"]) ).group( 1 )
+					#print "Tipo: " + tipo
+					if tipo == "ESPAÑA":
+						subService.download_subtitles(subSearch[0],i,None,self.downloadFolder,None,None)
+						flag = 1
+
+				if flag == 1:
+					writeToLog( "Found, removing...",2)
+					sendNotification("Subtitles for " + entry[0] + " S" + str(entry[1]).zfill(2) + "E" + str(entry[2]).zfill(2) + " downloaded")
+					self.subpending.remove(entry)
+		
--- tsd.py
+++ tsd.py
@@ -6,33 +6,28 @@ __license__ = """GPL v3. See LICENCE for full text.
 Includes software parts by XBMC team, and feedparser, check respective files for licence details"""
 
 import feedparser
-import re
 import os
+import re
 import ConfigParser
 import time
+from subtitles import Subtitles
 
+
+# Paths & files
+showFile = "shows.cfg"
+global logfile 
 logfile = "tvshowlog.txt"
-
 subfile = "subpen.txt"
-
-
+subfolder =	"/storage/USB/Descargas/Subtitles"
+downloadDir = "/var/lib/transmission-daemon/newtorrent/"
 #os.chdir ("/home/pi/tvshowsdownloader/")
 
 
-subfileo = open(subfile)
-subpending = []
-while True:
-	line = subfileo.readline()
-	if not line:
-		break
-		
-	parts = line.split(',')
-	subpending.append([parts[0], int(parts[1]), int(parts[2])])
-subfileo.close()
 
-
+#-------------------------------------------------------------------
 def sendNotification (text):
 	os.system("echo \"" + text + "\" | /usr/bin/sendxmpp -t -u ceoserverpi -o gmail.com jose.martin.burgos")
+#-------------------------------------------------------------------
 def writeToLog (text, level):
 	f = open ("logfile", "a")
 	
@@ -43,41 +38,48 @@ def writeToLog (text, level):
 	
 	print >> f, a + text
 	print  a + text
-
+	f.close()
 #-------------------------------------------------------------------
 
 if __name__=='__main__':
 	
-	
+	# Initialize subtitler
+	subtitler = Subtitles (subfile, subfolder)
+	subtitler.loadPendingSubs()
+
+	# Initial log traces
 	writeToLog("Runned at " + time.strftime("%d/%m/%Y %H:%M:%S"),0)
 	writeToLog("",0)
 	writeToLog("Checking for new episodes...",0)
 	
-	showFile = "shows.cfg"
-	downloadDir = "/var/lib/transmission-daemon/newtorrent/"
+	# Read show file
 	configFile = ConfigParser.ConfigParser()
 	configFile.read(showFile)
+
 	for show in configFile.sections():
 	
 		writeToLog(show,1)
 		writeToLog( "Downloading RSS file...",2)
-		python_wiki_rss_url = configFile.get(show, "rss")
+
+		showrssUrl = configFile.get(show, "rss")
 	
-		feed = feedparser.parse( python_wiki_rss_url )
+		feed = feedparser.parse( showrssUrl )
 	
 		last_chapter = int(configFile.get(show, "last_chapter"))
 		last_season = int(configFile.get(show, "last_season"))
-	
+		
+		# Try to download the RSS correctly
 		counter = 1
 		while feed["bozo"] == 1:
 			#writeToLog("Failed, retrying...")
 			feed = None
-			feed = feedparser.parse( python_wiki_rss_url )
+			feed = feedparser.parse( showrssUrl )
 			counter +=1
 			if counter == 11:
 				writeToLog("Cannot download RSS",2)
 				break
 
+		# Get chapter entries from the RSS
 		items = feed["items"]
 
 		flag = 0
@@ -101,16 +103,16 @@ if __name__=='__main__':
 			if (season > last_season or (season == last_season and chapter > last_chapter)) and flag==0:
 				writeToLog( "New episode: Season: " + str(season) + ". Chapter: " + str(chapter),2)
 				#print "Season " + str(chpp[0]) + " chapter " + chpp[1]
-				#print "Torrent: " + str(item["link"])
-				#os.system("wget -q " + str(item["link"]) + " -P " + downloadDir)
+				
+
 				os.system("transmission-remote --add \"" + item["link"] + "\" --auth transmission:esteesmiservidortorrent")
 			
 				#notify
 				chpstr= show + " S" + str(season).zfill(2) + "E" + str(chapter).zfill(2)
-				
 				sendNotification("Downloading " + chpstr)
+
 				#subs
-				subpending.append([show,season,chapter])
+				subtitler.addSub([show,season,chapter])
 			
 				if season > new_season or ((season == new_season) and (chapter > new_chapter)):
 					new_season = season
@@ -124,33 +126,12 @@ if __name__=='__main__':
 	with open(showFile, 'wb') as configWrite:
 		configFile.write(configWrite)
 
-writeToLog("",0)
-writeToLog("Checking for subtitles...",0)
-
-#print subpending
-for entry in subpending:
-	import subService
-	writeToLog( "Looking for sub to " + entry[0] + " " + str(entry[1]).zfill(2) + "x" + str(entry[2]).zfill(2),1)
-	a=subService.search_subtitles("", "", entry[0],"",str(entry[1]),str(entry[2]),"","","Spanish","","","")
-	if len(a[0]) > 0:
-		flag = 0
-		for i in range(len(a[0])):
-			tipo = re.compile( "\((.*)\)" ).search( str(a[0][i]["filename"]) ).group( 1 )
-			#print "Tipo: " + tipo
-			if tipo == "ESPAÑA":
-				subService.download_subtitles(a[0],i,None,"/storage/USB/Descargas/Subtitles",None,None)
-				flag = 1
-		if flag == 1:
-			writeToLog( "Found, removing...",2)
-			sendNotification("Subtitles for " + entry[0] + " S" + str(entry[1]).zfill(2) + "E" + str(entry[2]).zfill(2) + " downloaded")
-			subpending.remove(entry)
+	writeToLog("",0)
+	writeToLog("Checking for subtitles...",0)
+	
+	subtitler.checkSubtitles()
+	subtitler.writePendingSubs()
 
 
-
-subfileo = open(subfile,"w")
-for entry in subpending:
-	print >> subfileo, entry[0] + "," + str(entry[1]) + "," + str(entry[2])
-subfileo.close()
-writeToLog("Done",0)
-writeToLog("---------------------------------------",0)
-
+	writeToLog("Done",0)
+	writeToLog("---------------------------------------",0)
